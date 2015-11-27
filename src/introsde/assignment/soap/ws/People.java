0417471edf383bb9f3d5bf28e4aceb21c896fa96
rodrigosestari <sestari@Rodrigos-MacBook-Air.local>
2015-11-27 12:42:42 +0100

    persistence

--- src/introsde/assignment/soap/ws/People.java
+++ src/introsde/assignment/soap/ws/People.java
@@ -38,4 +38,19 @@ public interface People {
     @WebMethod(operationName="updatePersonHealthProfile")
     @WebResult(name="hpId") 
     public int updatePersonHP(@WebParam(name="personId") int id, @WebParam(name="healthProfile") LifeStatus hp);
+    
+    
+    
+    /*
+     Method #1: readPersonList() => List | should list all the people in the database (see below Person model to know what data to return here) in your database
+Method #2: readPerson(Long id) => Person | should give all the Personal information plus current measures of one Person identified by {id} (e.g., current measures means current healthProfile)
+Method #3: updatePerson(Person p) => Person | should update the Personal information of the Person identified by {id} (e.g., only the Person's information, not the measures of the health profile)
+Method #4: createPerson(Person p) => Person | should create a new Person and return the newly created Person with its assigned id (if a health profile is included, create also those measurements for the new Person).
+Method #5: deletePerson(Long id) should delete the Person identified by {id} from the system
+Method #6: readPersonHistory(Long id, String measureType) => List should return the list of values (the history) of {measureType} (e.g. weight) for Person identified by {id}
+Method #7: readMeasureTypes() => List should return the list of measures
+Method #8: readPersonMeasure(Long id, String measureType, Long mid) => Measure should return the value of {measureType} (e.g. weight) identified by {mid} for Person identified by {id}
+Method #9: savePersonMeasure(Long id, Measure m) =>should save a new measure object {m} (e.g. weight) of Person identified by {id} and archive the old value in the history
+Method #10: updatePersonMeasure(Long id, Measure m) => Measure | should update the measure identified with {m.mid}, related to the Person identified by {id}
+     */
 }
